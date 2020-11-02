0f1247d014b6a2a7dc74d5fce7bef57b3722e256
sirNugg3ts <diogopascoal789@gmail.com>
2020-11-02 18:49:05 +0000

    add 2 funcoes arbitro.c

--- arbitro.c
+++ arbitro.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
+#include  "arbitro.h"
 
-int main(){printf("Eu sou o arbitro\n");}
+int main(int argc, char **argv){
+
+
+
+}
+
+char *obtemGAMEDIR(){
+	return getenv("GAMEDIR");
+}
+
+int obtemMaxPlayers(){
+	return getenv("MAXPLAYERS");
+}
+
+int obtemDuracaoCampeonato(){}
+
+int obtemTempoEspera(){}
+
